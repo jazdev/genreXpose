289741a8886a3647cd307696ef01c235ad9799fb
Jasdev Singh <jasdev@live.in>
2014-05-25 01:21:23 -0700

    updated classification

--- genreXpose/classifier.py
+++ genreXpose/classifier.py
@@ -1,27 +1,23 @@
 import os
+import timeit
 import numpy as np
 from collections import defaultdict
-
-from sklearn.metrics import precision_recall_curve, roc_curve
+from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
 from sklearn.cross_validation import ShuffleSplit
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
-
 from utils import GENRE_LIST, GENRE_DIR, TEST_DIR
-from utils import plot_confusion_matrix, plot_roc
-
-from ceps import read_ceps, create_ceps_test, read_ceps_test
-
-from pydub import AudioSegment
+from utils import plot_confusion_matrix, plot_roc_curves
+from ceps import read_ceps, read_ceps_test
 
 genre_list = GENRE_LIST
 
-clf = None
-
-def train_model(clf_factory, X, Y, name, plot=False):
+def train_model(X, Y, name, plot=False):
     """
+        train_model(vector, vector, name[, plot=False])
+        
         Trains and saves model to disk.
     """
     labels = np.unique(Y)
@@ -39,7 +35,7 @@ def train_model(clf_factory, X, Y, name, plot=False):
     tprs = defaultdict(list)
     fprs = defaultdict(list)
 
-    clfs = []  # just to later get the median
+    clfs = []  # for the median
 
     cms = []
 
@@ -47,7 +43,6 @@ def train_model(clf_factory, X, Y, name, plot=False):
         X_train, y_train = X[train], Y[train]
         X_test, y_test = X[test], Y[test]
 
-        global clf
         clf = LogisticRegression()
         clf.fit(X_train, y_train)
         clfs.append(clf)
@@ -68,13 +63,6 @@ def train_model(clf_factory, X, Y, name, plot=False):
             proba = clf.predict_proba(X_test)
             proba_label = proba[:, label]
 
-            precision, recall, pr_thresholds = precision_recall_curve(
-                y_label_test, proba_label)
-            pr_scores[label].append(auc(recall, precision))
-            precisions[label].append(precision)
-            recalls[label].append(recall)
-            thresholds[label].append(pr_thresholds)
-
             fpr, tpr, roc_thresholds = roc_curve(y_label_test, proba_label)
             roc_scores[label].append(auc(fpr, tpr))
             tprs[label].append(tpr)
@@ -82,16 +70,15 @@ def train_model(clf_factory, X, Y, name, plot=False):
 
     if plot:
         for label in labels:
-            print("Plotting %s"%genre_list[label])
             scores_to_sort = roc_scores[label]
             median = np.argsort(scores_to_sort)[len(scores_to_sort) / 2]
             desc = "%s %s" % (name, genre_list[label])
-            #plot_roc(roc_scores[label][median], desc, tprs[label][median],fprs[label][median], label='%s vs rest' % genre_list[label])
+            plot_roc_curves(roc_scores[label][median], desc, tprs[label][median],fprs[label][median], label='%s vs rest' % genre_list[label])
 
     all_pr_scores = np.asarray(pr_scores.values()).flatten()
     summary = (np.mean(scores), np.std(scores),
                np.mean(all_pr_scores), np.std(all_pr_scores))
-    print("%.3f\t%.3f\t%.3f\t%.3f\t" % summary)
+    #print("%.3f\t%.3f\t%.3f\t%.3f\t" % summary)
 
     #save the trained model to disk
     joblib.dump(clf, 'saved_model/model_ceps.pkl')
@@ -100,21 +87,19 @@ def train_model(clf_factory, X, Y, name, plot=False):
 
 
 if __name__ == "__main__":
-
-    import timeit,os
     start = timeit.default_timer()
-    global traverse
-    for subdir, dirs, files in os.walk(GENRE_DIR):
-        traverse = list(set(dirs).intersection( set(GENRE_LIST) ))
-        break
-    print "Working with these genres --> ", traverse
-    print "Starting classification"    
+    print
+    print " Starting classification \n"
+    print " Classification running ... \n" 
     X, y = read_ceps(genre_list)
-    train_avg, test_avg, cms = train_model(None, X, y, "Log Reg CEPS", plot=True)
+    train_avg, test_avg, cms = train_model(X, y, "LogisticCEPS", plot=True)
     cm_avg = np.mean(cms, axis=0)
     cm_norm = cm_avg / np.sum(cm_avg, axis=0)
-    print "\nClassification finished \n"
+    print " Classification finished \n"
     stop = timeit.default_timer()
-    print "Total time taken (s) = ", (stop - start)
-    print "\nPlotting confusion matrix ...\n\n"
-    #plot_confusion_matrix(cm_norm, genre_list, "ceps","Confusion matrix of a CEPS based classifier")
+    print " Total time taken (s) = ", (stop - start)
+    print "\n Plotting confusion matrix ... \n"
+    plot_confusion_matrix(cm_norm, genre_list, "ceps","CEPS classifier - Confusion matrix")
+    print " All Done\n"
+    print " See plots in 'graphs' directory \n"
+    
