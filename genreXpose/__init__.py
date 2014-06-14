ec937e1e48a86e3cb3f95ecc021374f30691d9be
Jasdev Singh <jasdev@live.in>
2014-06-13 23:33:44 -0700

    removed setup

--- genreXpose/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ['tester', 'utils', 'ceps', 'classifier']
-
-from ceps import read_ceps_test, read_ceps, create_ceps_test, create_ceps
-
-from utils import GENRE_DIR, CHART_DIR, GENRE_LIST, convert_any_to_wav, convert_dataset_to_wav, plot_confusion_matrix, plot_roc_curves
